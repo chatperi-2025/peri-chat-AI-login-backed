2a3bb3c14b79cc8ec531bacbf08511773288e966
chatperi-2025 <chatperi2@gmail.com>
2025-04-27 01:44:28 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const cors = require('cors');
 
 app.use(cors({
-  origin: 'https://dummy-60040263906.development.catalystserverless.in/app/index.html' // ✅ Updated: only domain, no "/app/index.html"
+  origin: 'https://dummy-60040263906.development.catalystserverless.in' // ✅ Updated: only domain, no "/app/index.html"
 }));
 app.use(express.json());
 
